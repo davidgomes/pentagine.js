9c1b21d21172202efe1f16232e31ac07c2d1d480
David Gomes <davidrafagomes@gmail.com>
2012-12-09 18:29:30 +0000

    Forgot to add the files, here they are.

---
+++ example/PlayState2.js
@@ -0,0 +1,22 @@
+function PlayState() {
+  this.setup = function() {
+    this.balls = new SpriteList();
+
+    for (var i = 0; i < 10; i++) {
+      this.balls.push(new Sprite("ball.png", 5, i * 40 + 5));
+    }
+  }
+
+  this.update = function() {
+
+  }
+
+  this.draw = function() {
+    clearCanvas();
+
+    this.balls.draw();
+  }
+}
+
+var playState = new PlayState();
+switchState(playState);
