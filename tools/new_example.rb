b97fa3f1029d747f2b74cb7f4680a48220b82c68
David Gomes <davidrafagomes@gmail.com>
2013-01-27 16:53:54 +0000

    Started work on new_example script.

---
+++ tools/new_example.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+# Creates a new example
+
+print "Name: "
+name = gets.chomp
+print "\nGenerate index.html? [Y/n]"
+generate_html = gets.chomp
+print "\nAdd example to examples list? [Y/n]"
+add_to_list = gets.chomp
