b4fd399bb479de3d8c260d33354aca7cb05c10c6
David Gomes <davidrafagomes@gmail.com>
2013-01-27 19:38:52 +0000

    Finished new_example script.

--- tools/new_example.rb
+++ tools/new_example.rb
@@ -1,10 +1,54 @@
 #!/usr/bin/env ruby
 
-# Creates a new example
-
+# Get all the settings
 print "Name: "
 name = gets.chomp
-print "\nGenerate index.html? [Y/n]"
+print "Generate index.html? [Y/n] "
 generate_html = gets.chomp
-print "\nAdd example to examples list? [Y/n]"
+print "Add example to examples list? [Y/n] "
 add_to_list = gets.chomp
+
+# Create a folder
+folder_name = name.gsub(" ", "_").downcase
+Dir::mkdir("examples/" + folder_name)
+
+# Generate index.html
+if generate_html.downcase != "n" then
+  index_html = "<!DOCTYPE html><html>
+  <head>
+    <title>%s</title>
+    <meta charset=\"utf-8\">
+  </head>
+
+  <body style=\"margin: 0px;\">
+    <canvas id=\"canvas\" width=\"800\" height=\"640\">
+  </body>
+
+  <!-- Include Pentagine -->
+  <script src=\"../../src/stats.min.js\" type=\"text/javascript\"></script>
+  <script src=\"../../src/main.js\" type=\"text/javascript\"></script>
+  <script src=\"../../src/Drawing.js\" type=\"text/javascript\"></script>
+  <script src=\"../../src/Camera.js\" type=\"text/javascript\"></script>
+  <script src=\"../../src/Sprite.js\" type=\"text/javascript\"></script>
+  <script src=\"../../src/SpriteList.js\" type=\"text/javascript\"></script>
+  <script src=\"../../src/Input.js\" type=\"text/javascript\"></script>
+
+  <!-- Include game classes -->
+  <!-- <script src=\"PlayState.js\" type=\"text/javascript\"></script> -->
+</html>" % [name]
+
+  index_html_file = File.new("examples/" + folder_name + "/index.html", "w")
+  index_html_file.write(index_html + "\n")
+  index_html_file.close
+end
+
+# Add new example to examples list on examples/index.html
+if add_to_list.downcase != "n" then
+  old_html_list = File.open("examples/index.html").read().split("\n")
+  new_html_file = File.new("examples/index.html", "w")
+
+  new_line = "\n      <li><a href=\"%s\">%s</a></li>\n" % [folder_name + "/index.html", name]
+
+  new_html_file.write(old_html_list[0..11].join("\n") + new_line + old_html_list[12..old_html_list.length - 1].join("\n"))
+  new_html_file.close
+end
