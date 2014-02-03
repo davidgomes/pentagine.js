f68e7824387357eede3d26d2ac52db48ef335f33
David Gomes <davidrafagomes@gmail.com>
2014-02-03 22:03:44 +0000

    Fixed the global object issue.

--- demos/helicopter_game/PlayState.js
+++ demos/helicopter_game/PlayState.js
@@ -1,21 +1,21 @@
 function PlayState() {
   this.setup = function() {
-    this.helicopter = new Sprite("helicopter1.png", 100, context.height / 2 - 100);
+    this.helicopter = new penta.Sprite("helicopter1.png", 100, penta.context.height / 2 - 100);
     this.helicopter.vy = 5;
 
     this.walls = [];
-    this.walls[context.width - 1] = 20;
+    this.walls[penta.context.width - 1] = 20;
     this.speed = 30;
 
     this.difficulty = 50;
     this.score = 0;
-  }
+  };
 
   this.update = function() {
-    if (isDown("up") || isDown("w"))
+    if (penta.isDown("up") || penta.isDown("w"))
       this.helicopter.y -= 400 * this.dt;
 
-    if (isDown("down") || isDown("s"))
+    if (penta.isDown("down") || penta.isDown("s"))
       this.helicopter.y += 200 * this.dt;
 
     for (var i = 2; i < this.walls.length - this.speed; i++) {
@@ -24,7 +24,7 @@ function PlayState() {
       }
     }
 
-    if (this.walls.length == context.width) {
+    if (this.walls.length == penta.context.width) {
       if (this.walls[this.walls.length - 2] < 250) {
         if (Math.floor(Math.random() * 5) > 1)
           this.walls[this.walls.length - 1] = this.walls[this.walls.length - 2] + Math.floor(Math.random() * 5) + 1;
@@ -43,25 +43,25 @@ function PlayState() {
 
     this.difficulty += 0.1;
     this.score++;
-  }
+  };
 
   this.draw = function() {
-    clearCanvas();
+    penta.clearCanvas();
 
     currentFont = "10px arial";
-    drawString("Score: " + this.score.toString(), 2, 10, "#FFF");
-    drawString("Delta Time: " + Math.floor((this.dt * 1000).toString()) + "ms", 2, 20, "#FFF");
-    drawString("FPS: " + Math.floor((1 / this.dt).toString()) + "", 2, 30, "#FFF");
+    penta.drawString("Score: " + this.score.toString(), 2, 10, "#FFF");
+    penta.drawString("Delta Time: " + Math.floor((this.dt * 1000).toString()) + "ms", 2, 20, "#FFF");
+    penta.drawString("FPS: " + Math.floor((1 / this.dt).toString()) + "", 2, 30, "#FFF");
     
     for (var i = 0; i < this.walls.length; i++) {
-      drawRectangle(i, 0, 1, this.walls[i], "#123");
-      drawRectangle(i, context.height - this.walls[i], 1, this.walls[i], "#123");
+      penta.drawRectangle(i, 0, 1, this.walls[i], "#123");
+      penta.drawRectangle(i, penta.context.height - this.walls[i], 1, this.walls[i], "#123");
     }
 
     this.helicopter.draw();
-  }
+  };
 }
 
 desiredFPS = 30;
-preventKeys("down", "right", "left", "right", "space");
-switchState(new PlayState());
+penta.preventKeys("down", "right", "left", "right", "space");
+penta.switchState(new PlayState());
--- tools/merge.rb
+++ tools/merge.rb
@@ -2,9 +2,7 @@
 
 puts "Merging all pentagine's source files into pentagine.js.\n\n"
 
-files_names = ["src/Animation.js", "src/Drawing.js", "src/SpriteList.js",
-               "src/Camera.js", "src/Input.js", "src/Sprite.js",
-               "src/stats.min.js", "src/main.js"]
+files_names = ["src/main.js"]
 
 merged_file = File.new("build/pentagine.js", "w")
 
